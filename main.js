1052db189d2a1e0cde58e8e1dfa6534a6bd95f84
TravLord <Travisblord@gmail.com>
2021-11-19 12:33:17 -0700

    Pushing JavaScript Project Folder to remote repo

---
+++ main.js
@@ -0,0 +1,22 @@
+document.write('Hello,World');
+    var donkey = "    Strings for days.....";
+
+    document.write(donkey);      // first printing and box alert exammples
+
+    window.alert(donkey);
+document.write(' \"To be, or not to be? that is the question.\"')
+
+document.write('Adding togther sententences,' // concatenating string senctences togehter.
++ 'is easy as pudding pie!')  
+
+var catDog = ' cat ' + 'dog';  // concatenating strings example
+document.write(catDog);
+
+var general = ' general here' ,  generic = ' plain jane ' , identical= ' twins'; // Multiple variable and value assignment. 
+document.write(identical)
+var general = general.fontcolor('red');
+var generic = generic.fontcolor('yellow');     // These 3 lines of change the color of font for the above variables
+var identical = identical.fontcolor('purple');
+document.write(general,generic,identical);
+document.write('');
+document.write(14+9003);  // This is an statement printing an expression.
