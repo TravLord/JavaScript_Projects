d3a4385702599928cea7327ef077710f751807e6
TravLord <Travisblord@gmail.com>
2021-11-19 18:29:20 -0700

    Commiting fixes for assignment

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ document.write('Adding togther sententences,' // concatenating string senctences
 var catDog = ' cat ' + 'dog';  // concatenating strings example
 document.write(catDog);
 
-var general = ' general here' ,  generic = ' plain jane ' , identical= ' twins'; // Multiple variable and value assignment. 
+var general = ' general here' ,  generic = ' plain jane ' , identical= ' twins '; // Multiple variable and value assignment. 
 document.write(identical)
 var general = general.fontcolor('red');
 var generic = generic.fontcolor('yellow');     // These 3 lines of change the color of font for the above variables
@@ -20,3 +20,8 @@ var identical = identical.fontcolor('purple');
 document.write(general,generic,identical);
 document.write('');
 document.write(14+9003);  // This is an statement printing an expression.
+
+var get =' getters&';
+var set = 'setters';
+
+document.write(get + set);
