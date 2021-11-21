7d940111bde3caa4715ea127652fb023ea6f9630
TravLord <Travisblord@gmail.com>
2021-11-20 17:26:30 -0700

    Pushing Project2_functions folder

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function my1stFunction() {
+    var str = "This text is purple!";
+    var result = str.fontcolor(purple); 
+    document.getElementById("Purple_Text").innerHTML= result;
+}
+function my_function2() {
+    var con = "Come together";
+    con += " right now, over me.";
+    document.getElementById("concatenate").innerHTML = con;
+
+}
